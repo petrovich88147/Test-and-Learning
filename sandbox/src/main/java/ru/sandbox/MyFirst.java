eb961e0394feeec1e4f63d67be122d99d983d149
petrovich88147 <79191338+petrovich88147@users.noreply.github.com>
2021-02-20 00:18:59 +0300

    познакомился с переменными

--- sandbox/src/main/java/ru/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/sandbox/MyFirst.java
@@ -4,5 +4,8 @@ public class MyFirst {
 
   public static void main(String[] args) {
     System.out.println("Hello, world");
+    int l = 7;
+    int s = l*l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s );
   }
 }

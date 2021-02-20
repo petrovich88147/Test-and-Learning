2c3e7f476873825440cdcc91087274f4465e3ae9
petrovich88147 <79191338+petrovich88147@users.noreply.github.com>
2021-02-20 15:13:29 +0300

    добавлено несколько функций

--- sandbox/src/main/java/ru/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/sandbox/MyFirst.java
@@ -1,11 +1,29 @@
 package ru.sandbox;
 
+
 public class MyFirst {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
-    int l = 7;
-    int s = l*l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s );
+    hello("World");
+
+    double len =5;
+    System.out.println("Площадь квадрата со стороной " + len +" = " + area (len));
+
+    double a = 20;
+    double b = 2;
+    System.out.println("Площадь прямоугольника со сторонами " + a +" и " +b+ " = " + area (a,b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody);
+  }
+
+  public static double area(double l) {
+    return l*l;
+
+  }
+  public static double area(double a, double b){
+    return a*b;
   }
 }

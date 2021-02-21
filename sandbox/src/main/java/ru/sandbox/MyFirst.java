c2e4359ea733c89a7efe58bd5e55092fcfac44cd
petrovich88147 <79191338+petrovich88147@users.noreply.github.com>
2021-02-21 12:22:21 +0300

    Классы объекта

--- sandbox/src/main/java/ru/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/sandbox/MyFirst.java
@@ -6,12 +6,11 @@ public class MyFirst {
   public static void main(String[] args) {
     hello("World");
 
-    double len =5;
-    System.out.println("Площадь квадрата со стороной " + len +" = " + area (len));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-    double a = 20;
-    double b = 2;
-    System.out.println("Площадь прямоугольника со сторонами " + a +" и " +b+ " = " + area (a,b));
+    Rectangle r = new Rectangle(4,5);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
   }
 
@@ -19,11 +18,12 @@ public class MyFirst {
     System.out.println("Hello," + somebody);
   }
 
-  public static double area(double l) {
-    return l*l;
+  public static double area(Square s) {
+    return s.l * s.l;
 
   }
-  public static double area(double a, double b){
-    return a*b;
+
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 }
